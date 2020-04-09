9187f7f250b37e44507ec9fc85f2416a81b47601
Joey Hewitt <joey@joeyhewitt.com>
2020-04-09 01:07:25 -0600

    apache webdav: stop using deprecated subservice cfg

---
+++ services/apache-httpd/vhostsset.nix
@@ -0,0 +1,23 @@
+{ lib, config, modulesPath, ... }:
+let
+inherit (lib) mkOption mkForce;
+inherit (lib) types;
+in
+{
+	config.services.httpd.virtualHosts = lib.mkAfter (lib.attrsets.attrValues config.services.httpd.virtualHostsSet);
+
+	options = {
+		services.httpd.virtualHostsSet = mkOption {
+			type = types.attrsOf (types.submodule {
+				options = import (modulesPath+"/services/web-servers/apache-httpd/per-server-options.nix") {
+					inherit lib;
+					forMainServer = false;
+				};
+			});
+			description = ''
+				Modules that want to build a vhost together can assign to this property, and the merged vhost definition
+				will be appended to the virtualHosts list.
+			'';
+		};
+	};
+}
