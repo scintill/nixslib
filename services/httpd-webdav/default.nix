6b8cd3eeb67606e52772db5ce51f1d9f69df32e4
Joey Hewitt <joey@joeyhewitt.com>
2020-04-10 23:19:07 -0600

    expand tabs

--- services/httpd-webdav/default.nix
+++ services/httpd-webdav/default.nix
@@ -12,71 +12,71 @@ virtualHost = config.services.httpd.virtualHostsSet.${cfg.virtualHostKey};
 webdavStateDir = "${httpdCfg.stateDir}/webdav/${virtualHost.hostName}";
 in
 {
-	imports = [
-		../apache-httpd/vhostsset.nix
-	];
+    imports = [
+        ../apache-httpd/vhostsset.nix
+    ];
 
-	config = {
-		services.httpd = {
-			enable = true;
-			virtualHostsSet.${cfg.virtualHostKey}.extraConfig =
-				# Digest auth could help, but I can't use it with my usecase, so I'm not bothering for now.
-				lib.mkAssert (virtualHost.enableSSL || cfg.allowPlaintextAuth) "SSL is required by httpd-webdav, unless you set allowPlaintextAuth"
-				''
-					DAVLockDB ${webdavStateDir}/lock
+    config = {
+        services.httpd = {
+            enable = true;
+            virtualHostsSet.${cfg.virtualHostKey}.extraConfig =
+                # Digest auth could help, but I can't use it with my usecase, so I'm not bothering for now.
+                lib.mkAssert (virtualHost.enableSSL || cfg.allowPlaintextAuth) "SSL is required by httpd-webdav, unless you set allowPlaintextAuth"
+                ''
+                    DAVLockDB ${webdavStateDir}/lock
 
-					Alias ${cfg.baseUrl} ${cfg.storageDir}
-					<Location ${cfg.baseUrl}>
-						DAV On
-						AuthType Basic
-						AuthName "WebDAV"
-						AuthBasicProvider file
-						AuthUserFile ${cfg.authUserFile}
-						Require valid-user
-					</Location>
-					<Directory ${cfg.storageDir}>
-						Options +Indexes
-						Require all granted
-						AllowOverride None
-					</Directory>
-				'';
-		};
-		systemd.services.httpd-webdav-init = {
-			wantedBy = [ "multi-user.target" ];
-			before = [ "httpd.service" ];
-			script = ''
-				mkdir -m 0770 -p ${webdavStateDir}
-				[ $(id -u) != 0 ] || chown ${with httpdCfg; "${user}:${group}"} ${webdavStateDir}
-			'';
-		};
-	};
+                    Alias ${cfg.baseUrl} ${cfg.storageDir}
+                    <Location ${cfg.baseUrl}>
+                        DAV On
+                        AuthType Basic
+                        AuthName "WebDAV"
+                        AuthBasicProvider file
+                        AuthUserFile ${cfg.authUserFile}
+                        Require valid-user
+                    </Location>
+                    <Directory ${cfg.storageDir}>
+                        Options +Indexes
+                        Require all granted
+                        AllowOverride None
+                    </Directory>
+                '';
+        };
+        systemd.services.httpd-webdav-init = {
+            wantedBy = [ "multi-user.target" ];
+            before = [ "httpd.service" ];
+            script = ''
+                mkdir -m 0770 -p ${webdavStateDir}
+                [ $(id -u) != 0 ] || chown ${with httpdCfg; "${user}:${group}"} ${webdavStateDir}
+            '';
+        };
+    };
 
-	options = with lib; {
-		services.httpd-webdav = {
-			virtualHostKey = mkOption {
-				type = types.str;
-			};
-			storageDir = mkOption {
-				type = types.path;
-				description = ''
-					Filesystem path where WebDAV-accessible files are written/read. Should be created externally and owned
-					by the user/group that the server runs as.
-				'';
-			};
-			authUserFile = mkOption {
-				type = types.path;
-				description = "Path to file containing auth credentials created with htdigest. Should be readable by the server's user/group.";
-			};
-			baseUrl = mkOption {
-				type = types.str;
-				description = "Base URL under which WebDAV is enabled.";
-				example = "/webdav";
-			};
-			allowPlaintextAuth = mkOption {
-				type = types.bool;
-				description = "Set to true to disable assertion failure when SSL is not enabled. This means auth will be plaintext, which is not secure for many uses.";
-				default = false;
-			};
-		};
+    options = with lib; {
+        services.httpd-webdav = {
+            virtualHostKey = mkOption {
+                type = types.str;
+            };
+            storageDir = mkOption {
+                type = types.path;
+                description = ''
+                    Filesystem path where WebDAV-accessible files are written/read. Should be created externally and owned
+                    by the user/group that the server runs as.
+                '';
+            };
+            authUserFile = mkOption {
+                type = types.path;
+                description = "Path to file containing auth credentials created with htdigest. Should be readable by the server's user/group.";
+            };
+            baseUrl = mkOption {
+                type = types.str;
+                description = "Base URL under which WebDAV is enabled.";
+                example = "/webdav";
+            };
+            allowPlaintextAuth = mkOption {
+                type = types.bool;
+                description = "Set to true to disable assertion failure when SSL is not enabled. This means auth will be plaintext, which is not secure for many uses.";
+                default = false;
+            };
+        };
     };
 }
